ee0079187b0c73c882e1889f5b73cdda5669b653
Zemlyakov Stas <redtom@yandex.ru>
2019-01-24 16:45:40 +0300

    Update Author.kt

--- src/main/kotlin/com/github/rcd27/demo/model/Author.kt
+++ src/main/kotlin/com/github/rcd27/demo/model/Author.kt
@@ -2,6 +2,8 @@ package com.github.rcd27.demo.model
 
 import javax.persistence.*
 
+// FIXME: using data classes can be complicated for Hibernate.
+// see: https://stackoverflow.com/questions/35847763/kotlin-data-class-bean-validation-jsr-303
 @Entity
 data class Author(
         @Id
